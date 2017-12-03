17742eeac9f962e173d3cf741a8dcd16e2905e35
unknown <honza.klu@gmail.com>
2017-12-03 02:31:02 +0100

    Updated install scripts.

--- linux_install.sh
+++ linux_install.sh
@@ -1,3 +1,6 @@
+sudo apt-get -y install python3-venv
 python3 -m venv ./venv
 . ./venv/bin/activate
 pip install -r requirements.txt
+mkdir ~/.jupyter/
+echo "c.NotebookApp.ip = '*'" > ~/.jupyter/jupyter_notebook_config.py
--- linux_install_cuda.sh
+++ linux_install_cuda.sh
@@ -1,4 +1,4 @@
-sudo apt-get install build-essential
+sudo apt-get -y install build-essential
 
 #wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
 #sudo dpkg -i cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64=deb
@@ -11,9 +11,12 @@ sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 sudo apt-get update
 sudo apt-get install cuda-8
 
-wget klusacek.tk/~honza/data_archive/cudnn-8.0-linux-x64-v7.tgz
-tar -zxf cudnn-8.0-linux-x64-v7.tgz
+#wget klusacek.tk/~honza/data_archive/cudnn-8.0-linux-x64-v7.tgz
+wget klusacek.tk/~honza/data_archive/cudnn-8.0-linux-x64-v6.0.tgz
+tar -zxf cudnn-8.0-linux-x64-v6.0.tgz
 cd cuda
+sudo mkdir -p /usr/local/cuda/lib64/
+sudo mkdir -p /usr/local/cuda/include/
 sudo cp lib64/* /usr/local/cuda/lib64/
 sudo cp include/* /usr/local/cuda/include/
 #configure right speed
