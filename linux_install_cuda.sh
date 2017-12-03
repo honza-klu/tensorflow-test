51b5e6b2abcec9d45f532ebcab401f700c394fb9
unknown <honza.klu@gmail.com>
2017-12-03 22:53:33 +0100

    Changes to linux_install_cuda.sh to use /tmp/

--- linux_install_cuda.sh
+++ linux_install_cuda.sh
@@ -1,3 +1,5 @@
+ID=$RANDOM
+TMP_PATH="/tmp/${ID}"
 sudo apt-get -y install build-essential
 
 #wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
@@ -6,19 +8,19 @@ sudo apt-get -y install build-essential
 #sudo apt-get update
 #sudo apt-get install cuda
 
-wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
-sudo dpkg -i cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+wget -P $TMP_PATH http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
+sudo dpkg -i ${TMP_PATH}cuda-repo-ubuntu1604_8.0.61-1_amd64.deb
 sudo apt-get update
 sudo apt-get install cuda-8-0
 
 #wget klusacek.tk/~honza/data_archive/cudnn-8.0-linux-x64-v7.tgz
-wget klusacek.tk/~honza/data_archive/cudnn-8.0-linux-x64-v6.0.tgz
-tar -zxf cudnn-8.0-linux-x64-v6.0.tgz
-cd cuda
+wget -P $TMP_PATH klusacek.tk/~honza/data_archive/cudnn-8.0-linux-x64-v6.0.tgz -C ${TMP_PATH}
+tar -zxf cudnn-8.0-linux-x64-v6.0.tgz -C ${TMP_PATH}
+
 sudo mkdir -p /usr/local/cuda/lib64/
 sudo mkdir -p /usr/local/cuda/include/
-sudo cp lib64/* /usr/local/cuda/lib64/
-sudo cp include/* /usr/local/cuda/include/
+sudo cp ${TMP_PATH}/cuda/lib64/* /usr/local/cuda/lib64/
+sudo cp ${TMP_PATH}/cuda/include/* /usr/local/cuda/include/
 # CUDA Toolkit
 echo "export CUDA_HOME=/usr/local/cuda-8.0" >> ~/.bashrc
 echo "export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:$LD_LIBRARY_PATH" >> ~/.bashrc
