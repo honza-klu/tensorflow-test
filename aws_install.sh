d74e0dce0fd215646ec5a961bb8b918f82333180
unknown <honza.klu@gmail.com>
2017-12-18 01:30:31 +0100

    Added change directory to users home into aws_install.sh

--- aws_install.sh
+++ aws_install.sh
@@ -3,6 +3,7 @@ echo "INIT STARTED"
 
 #instal application as ordinary user to his home directory
 sudo -H -u ubuntu /bin/bash << 'EOF'
+cd ~
 
 #get sources
 git clone https://github.com/honza-klu/tensorflow-test
